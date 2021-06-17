ca151ff0bb4789cc16929a467c9dc16b68b663c7
Sujithvamshi <19211a04n4@bvrit.ac.in>
2021-06-17 17:35:06 +0530

    update

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask,request,render_template
-import pickle
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
-model_file = pickle.load(open('./model/model.pkl','rb'))
-def recommender(name,model_file):
-    df = pd.read_csv('model/movies.csv')
-    df['title']=df['title'].str.lower()
-    df.sort_values(by=('year'),axis=0,inplace=True,ascending=False) 
-    df.reset_index(drop=True,inplace=True) 
+
+df = pd.read_csv('model/movies.csv')
+df['genre'] = df['genre'].str.replace(',',' ')
+df['director']=df['director'].str.replace(' ','')
+df['cast']=df['cast'].str.replace(' ','')
+df['cast']=df['cast'].str.replace(',',' ')
+df['title']=df['title'].str.lower()
+df.sort_values(by=('year'),axis=0,inplace=True,ascending=False) 
+df.reset_index(drop=True,inplace=True) 
+df['year'] = df['year'].astype(str)
+df['features']=df['year']+" "+df['genre']+" "+df['country']+" "+df['cast']
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df['features'])
+sim = cosine_similarity(count_matrix)
+
+def recommender(name):
     name = name.lower()
     if name not in df['title'].unique():
         return ('Try Aboslute Spelling or it may not be in our database')
     else:
         i = df.loc[df['title']==name].index[0]
-        mov_id = list(enumerate(model_file[i]))
+        mov_id = list(enumerate(sim[i]))
         mov_id = sorted(mov_id,key = lambda x:x[1],reverse=True)
         mov_id = mov_id[1:11]
         rec_mov = []
@@ -23,14 +35,14 @@ def recommender(name,model_file):
             rec_mov.append(df['title'][a])
         return rec_mov
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('main.html')
 
 @app.route('/predict', methods=["GET","POST"])
 def predict():
     movie_name = request.form.get('movie')
-    predictions = recommender(movie_name,model_file)
+    predictions = recommender(movie_name)
     return render_template('trueReturn.html', predictions=predictions)
     
 if __name__=='__main__':
